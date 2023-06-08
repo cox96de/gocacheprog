afd9a9a78a12c5aca51edf5e22bd1ecaa5c8b2ad
xiazihao <xiazihao@bytedance.com>
2023-06-09 00:42:41 +0800

    feat: support s3 cache

--- cmd/gocacheprog/main.go
+++ cmd/gocacheprog/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/cox96de/gocacheprog/cache/disk"
+	"github.com/cox96de/gocacheprog/cache/s3"
 	"github.com/cox96de/gocacheprog/protocol"
 	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
@@ -14,23 +15,38 @@ type Flags struct {
 	Disk  struct {
 		Dir string
 	}
+	S3 struct {
+		AccessKey string
+		SecretKey string
+		Endpoint  string
+		Bucket    string
+		Region    string
+	}
 }
 
 func parseFlags(args []string) (*Flags, error) {
 	f := &Flags{}
 	flagSet := pflag.NewFlagSet("gocacheprog", pflag.ContinueOnError)
-	flagSet.StringVar(&f.Cache, "cache", "", "cache type, support disk")
-	flagSet.StringVar(&f.Disk.Dir, "disk-dir", "", "disk cache dir")
+	flagSet.StringVar(&f.Cache, "cache", "disk", "cache type, support disk and s3")
+	flagSet.StringVar(&f.Disk.Dir, "disk-dir", "/tmp/gocacheprog", "disk cache dir")
+	flagSet.StringVar(&f.S3.AccessKey, "s3-access-key", "", "s3 access key")
+	flagSet.StringVar(&f.S3.SecretKey, "s3-secret-key", "", "s3 secret key")
+	flagSet.StringVar(&f.S3.Endpoint, "s3-endpoint", "", "s3 endpoint")
+	flagSet.StringVar(&f.S3.Bucket, "s3-bucket", "", "s3 bucket")
+	flagSet.StringVar(&f.S3.Region, "s3-region", "default", "s3 region")
 	err := flagSet.Parse(args)
 	if err != nil {
+		flagSet.Usage()
 		return f, err
 	}
-	return f, nil
+	return f, err
 }
 
 func main() {
 	flags, err := parseFlags(os.Args)
-	checkError(err)
+	if err != nil {
+		os.Exit(1)
+	}
 	cache, err := composeCache(flags)
 	checkError(err)
 	err = protocol.Run(context.Background(), os.Stdin, os.Stdout,
@@ -40,6 +56,14 @@ func main() {
 
 func composeCache(c *Flags) (protocol.Handler, error) {
 	switch c.Cache {
+	case "s3":
+		return s3.NewCache(&s3.Option{
+			AccessKey: c.S3.AccessKey,
+			SecretKey: c.S3.SecretKey,
+			Endpoint:  c.S3.Endpoint,
+			Bucket:    c.S3.Bucket,
+			Region:    c.S3.Region,
+		})
 	case "disk":
 		return disk.NewDiskCache(c.Disk.Dir), nil
 	default:
--- go.mod
+++ go.mod
@@ -4,8 +4,15 @@ go 1.18
 
 require (
 	github.com/pkg/errors v0.9.1
-	github.com/spf13/pflag v1.0.5
 	gotest.tools/v3 v3.4.0
 )
 
-require github.com/google/go-cmp v0.5.5 // indirect
+require (
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/spf13/pflag v1.0.5
+)
+
+require (
+	github.com/aws/aws-sdk-go v1.44.276
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
